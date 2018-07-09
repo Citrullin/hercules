e373d68e011bb42fa50419709acd91425d5e7af0
muXxer <mux3r@web.de>
2018-07-10 00:52:55 +0200

    Fixed "panic: invalid argument to Intn"

--- tangle/outgoing.go
+++ tangle/outgoing.go
@@ -324,10 +324,9 @@ func getOldPending(excludeAddress string) *PendingRequest {
 		max = 1000
 	}
 
-	var l int
-	l = len(pendingRequests)
-	if l < max {
-		max = l
+	length := len(pendingRequests)
+	if length < max {
+		max = length
 	}
 
 	for i := 0; i < max; i++ {
@@ -352,21 +351,22 @@ func getAnyRandomOldPending(excludeAddress string) *PendingRequest {
 		max = 3000
 	}
 
-	var l int
-	l = len(pendingRequests)
-	if l < max {
-		max = l
+	length := len(pendingRequests)
+	if length < max {
+		max = length
 	}
 
-	start := utils.Random(0, max)
+	if max > 0 {
+		start := utils.Random(0, max)
 
-	for i := start; i < max; i++ {
-		k := randmap.FastKey(pendingRequests)
-		v := pendingRequests[k.(string)]
-		if v.LastNeighborAddr != excludeAddress {
-			v.LastTried = time.Now()
-			v.LastNeighborAddr = excludeAddress
-			return v
+		for i := start; i < max; i++ {
+			k := randmap.FastKey(pendingRequests)
+			v := pendingRequests[k.(string)]
+			if v.LastNeighborAddr != excludeAddress {
+				v.LastTried = time.Now()
+				v.LastNeighborAddr = excludeAddress
+				return v
+			}
 		}
 	}
 
