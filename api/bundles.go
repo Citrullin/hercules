37da8743dc80c99f5a86d02c45d68b8de3278bcd
Bruno A. dos Santos <brunoamancio.ti@gmail.com>
2018-07-17 18:13:30 +0200

    Converted address to Trytes for error message

--- api/bundles.go
+++ api/bundles.go
@@ -47,11 +47,13 @@ func storeAndBroadcastTransactions(request Request, c *gin.Context, broadcast bo
 			if tx.Value != 0 {
 				balance, err := db.GetInt64(db.GetAddressKey(tx.Address, db.KEY_BALANCE), nil)
 				if err != nil {
-					return errors.New("Could not read address' balance. Address: " + tx.Address + " Message: " + err)
+					addressTrytes := convert.BytesToTrytes(tx.Address)
+					return errors.New("Could not read address' balance. Address: " + addressTrytes + " Message: " + err)
 				} else if balance <= 0 || balance < tx.Value {
+					addressTrytes := convert.BytesToTrytes(tx.Address)
 					// TODO: collect values from all TXs, map to addesses and check for the whole sum
 					// This is as to prevent multiple partial transactions from the same address in the bundle
-					return errors.New("Insufficient balance. Sender address: " + tx.Address + " Balance: " + balance + " Tx value: " + tx.Value)
+					return errors.New("Insufficient balance. Sender address: " + addressTrytes + " Balance: " + balance + " Tx value: " + tx.Value)
 				}
 			}
 
