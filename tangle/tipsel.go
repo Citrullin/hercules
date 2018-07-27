39782f59f71c1f1366c69a80139692ab50cc2098
Roman <roman.semko@gmail.com>
2018-07-27 07:30:16 +0200

    Improved tipsel to do 100% conf rate independent of tangle size & speed

--- tangle/tipsel.go
+++ tangle/tipsel.go
@@ -19,8 +19,8 @@ const (
 	MaxTipselDepth      = 15
 	MaxCheckDepth       = 50
 	MaxTipAge           = MaxTipselDepth * time.Duration(40) * time.Second
-	tipAlpha            = 0.01
-	maxTipSearchRetries = 100
+	tipAlpha            = 0.001
+	maxTipSearchRetries = 40
 )
 
 // 1. Get reference: either one provided or latest milestone - 15 milestones back
@@ -197,7 +197,7 @@ func calculateRating(graph *GraphNode, seenKeys map[string][]byte) int {
 
 // 3. Walk the graph
 
-func walkGraph(rating *GraphRating, ratings map[string]*GraphRating, exclude map[string]bool) *GraphRating {
+func walkGraph(rating *GraphRating, ratings map[string]*GraphRating, exclude map[string]bool, takeInner bool) *GraphRating {
 	if rating.Graph.Children == nil {
 		if rating.Graph.Index == 0 {
 			return rating
@@ -238,20 +238,17 @@ func walkGraph(rating *GraphRating, ratings map[string]*GraphRating, exclude map
 		}
 		if randomNumber <= 0 {
 			// 3. Select random child
-			graph := walkGraph(ratings[string(child.Key)], ratings, exclude)
+			graph := walkGraph(ratings[string(child.Key)], ratings, exclude, takeInner)
 			if graph != nil {
 				return graph
 			}
 		}
 	}
-	return nil
-	/*/
-	if rating.Graph.Index == 0 {
+	if takeInner && rating.Graph.Index == 0 {
 		return rating
 	} else {
 		return nil
 	}
-	/**/
 }
 
 func GetTXToApprove(reference []byte, depth int) [][]byte {
@@ -277,7 +274,7 @@ func GetTXToApprove(reference []byte, depth int) [][]byte {
 	var results = make(map[string][]byte)
 	var exclude = make(map[string]bool)
 	for x := 0; x < maxTipSearchRetries; x += 1 {
-		r := walkGraph(graphRatings[string(reference)], graphRatings, exclude)
+		r := walkGraph(graphRatings[string(reference)], graphRatings, exclude, x > maxTipSearchRetries / 2)
 		if r != nil {
 			exclude[string(r.Graph.Key)] = true
 			hash, err := db.GetBytes(db.AsKey(r.Graph.Key, db.KEY_HASH), nil)
