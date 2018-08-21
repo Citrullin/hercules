50e2881a843d269e9d23aebd119dabdcc3840647
muXxer <mux3r@web.de>
2018-08-21 10:03:14 +0200

    Only seed the RNG once

--- utils/utils.go
+++ utils/utils.go
@@ -7,14 +7,18 @@ import (
 	"time"
 )
 
+var rng *rand.Rand
 var randLock = &sync.Mutex{}
+
+func init() {
+	rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+}
 
 func Random(min, max int) int {
 	// Need to lock this on smaller devices: https://github.com/golang/go/issues/3611
 	randLock.Lock()
 	defer randLock.Unlock()
-	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(max-min) + min
+	return rng.Intn(max-min) + min
 }
 
 func CreateDirectory(directoryPath string) error {
