530d1e6d9e5c7ef5d200e3b1152e373362f7dea9
Philipp Brüll <pb@simia.tech>
2018-08-24 10:07:30 +0200

    use stdlib utc timezone

--- utils/utils.go
+++ utils/utils.go
@@ -1,25 +1,24 @@
 package utils
 
 import (
-	"time"
 	"math/rand"
-	"sync"
 	"os"
+	"sync"
+	"time"
 )
 
 var RandLocker = &sync.Mutex{}
-var utcLocation, _ = time.LoadLocation("UTC")
 
-func Random (min, max int) int {
+func Random(min, max int) int {
 	// Need to lock this on smaller devices: https://github.com/golang/go/issues/3611
 	RandLocker.Lock()
 	defer RandLocker.Unlock()
 	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(max - min) + min
+	return rand.Intn(max-min) + min
 }
 
 func CreateDirectory(directoryPath string) error {
-	return os.MkdirAll(directoryPath,0777)
+	return os.MkdirAll(directoryPath, 0777)
 }
 
 func GetHumanReadableTime(timestamp int) string {
@@ -27,6 +26,6 @@ func GetHumanReadableTime(timestamp int) string {
 		return ""
 	} else {
 		unitxTime := time.Unix(int64(timestamp), 0)
-		return unitxTime.In(utcLocation).Format(time.RFC822)
+		return unitxTime.In(time.UTC).Format(time.RFC822)
 	}
 }
