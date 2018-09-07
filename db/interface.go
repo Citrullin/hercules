ccacce03f0dec7cae19591a85cb4d3b2b490868b
Philipp Brüll <pb@simia.tech>
2018-09-07 09:05:15 +0200

    removed dependency from db to db/coding

--- db/interface.go
+++ db/interface.go
@@ -3,8 +3,6 @@ package db
 import (
 	"fmt"
 
-	"./coding"
-
 	"github.com/spf13/viper"
 )
 
@@ -20,8 +18,10 @@ func RegisterImplementation(name string, constructor Constructor) {
 type Constructor func(*viper.Viper) (Interface, error)
 
 type Manipulator interface {
-	coding.PutGetRemover
+	GetBytes([]byte) ([]byte, error)
+	PutBytes([]byte, []byte) error
 	HasKey([]byte) bool
+	Remove([]byte) error
 	RemovePrefix([]byte) error
 	RemoveKeyCategory(byte) error
 	CountKeyCategory(byte) int
